6b9f43ac1591941b300dc05d88fc84fbb1e31987
ATHARAIF <abonsapi0106@gmail.com>
2025-07-14 03:35:52 +0700

    feat: Menambahkan fitur otentikasi pengguna Menambahkan halaman login dan fungsionalitas otentikasi

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    //
+    public function index(){
+        return view('auth.login');
+    }
+
+    public function handleLogin(Request $request){
+        $credentials=$request->validate([
+            'email'=>'required|email',
+            'password'=> 'required'
+        ],[
+            'email.required'=> 'Email harus diisi',
+            'email.email'=> 'Email tidak valid',
+            'password.required'=>'Password harus diisi'
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+        $user = User::where('email',$request->email)->first();
+        if(!$user){
+            // jika email tidak ada di database
+            return back()->withErrors([
+                'email'=> 'Email tidak sesuai'
+            ])->onlyInput('email');//untuk mempertahankan input email yang telah diisi
+        }else{
+            return back()->withErrors([
+            'password'=>'Password salah'
+            ])->onlyInput('email');//untuk mempertahankan input email yang telah diisi
+        }
+
+    }
+}
